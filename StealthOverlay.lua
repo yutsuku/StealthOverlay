e099ac363e928925921fbc10f911a0cb308ad98b
moh <moh@yutsuku.net>
2015-08-14 19:58:17 +0200

    initial release

---
+++ StealthOverlay.lua
@@ -0,0 +1,107 @@
+-- CONFIG SECTION --
+STEALTHOVERLAY_SHOWTIME = 2;
+STEALTHOVERLAY_FADETIME = 0.05;
+-- END --
+
+SlashCmdList["STEALTHOVERLAY_SLASHCMD"] = function(msg)
+	if ( msg == "" ) then
+		StealthOverlay_Stealth(3);
+	else
+		StealthOverlay_Stealth(tonumber(msg));
+	end
+end
+SLASH_STEALTHOVERLAY_SLASHCMD1 = '/caststealth'
+SLASH_STEALTHOVERLAY_SLASHCMD2 = '/stealth'
+
+function StealthOverlay_OnLoad()
+	this:RegisterEvent("PLAYER_ENTERING_WORLD");
+	this:RegisterEvent("COMBAT_TEXT_UPDATE"); -- from Blizzard_CombatText.lua
+	DEFAULT_CHAT_FRAME:AddMessage("StealthOverlay loaded");
+end
+
+function StealthOverlay_OnEvent(event, arg1, arg2)
+	
+	if event == "COMBAT_TEXT_UPDATE" and arg1 == "AURA_START" and arg2 == "Stealth" then
+		StealthOverlayFrame.display=true;
+		StealthOverlayFrame.onload=false;
+		StealthOverlayFrame.timer=STEALTHOVERLAY_SHOWTIME;
+		StealthOverlayFrame:SetAlpha(0);
+		StealthOverlayFrame:Show();
+	end
+	
+	if event == "COMBAT_TEXT_UPDATE" and arg1 == "AURA_END" and arg2 == "Stealth" then
+		StealthOverlayFrame.display=false;
+		StealthOverlayFrame.onload=false;
+		StealthOverlayFrame.timer=STEALTHOVERLAY_FADETIME; 
+		StealthOverlayFrame:SetAlpha(1);
+		StealthOverlayFrame:Show();
+	end
+	
+	if event == "PLAYER_ENTERING_WORLD" then
+		--[[local icon, i
+		for i=0,26 do -- from BuffFrame.xml
+			icon = UnitBuff("player", i)
+			if icon == "Interface\\Icons\\Ability_Stealth" then
+				StealthOverlayFrame.display=true;
+				StealthOverlayFrame.onload=true;
+				StealthOverlayFrame.timer=STEALTHOVERLAY_SHOWTIME;
+				StealthOverlayFrame:SetAlpha(1);
+				StealthOverlayFrame:Show();
+			end
+		end]]--
+		local _,_,isActive=GetShapeshiftFormInfo(1);
+		if isActive then
+			StealthOverlayFrame.display=true;
+			StealthOverlayFrame.onload=true;
+			StealthOverlayFrame.timer=STEALTHOVERLAY_SHOWTIME;
+			StealthOverlayFrame:SetAlpha(1);
+			StealthOverlayFrame:Show();
+		end
+	end
+	
+end
+
+function StealthOverlay_Stealth(t)
+	local _,_,s=GetShapeshiftFormInfo(1);
+	local i=1
+	if not s then
+		__LST = time()
+		CastSpellByName("Stealth");
+	else
+		if __LST ~= nil then
+			if __LST+t < time() then
+				while UnitBuff("player",i) do
+					if strfind(UnitBuff("player",i),"Stealth") then
+						CancelPlayerBuff(i-1);
+						break
+					end
+				end
+			end
+		else
+			while UnitBuff("player",i) do
+				if strfind(UnitBuff("player",i),"Stealth") then
+					CancelPlayerBuff(i-1);
+					break
+				end
+			end
+		end
+	end
+end
+
+function StealthOverlay_OnUpdate(arg1)
+	this.timer = this.timer - arg1;
+	if this.timer <= 0 then
+		if not this.display then
+			this:Hide();
+		end
+	elseif this.timer > 0 then
+		if this.onload then return end
+		-- fade in
+		if this.display then
+			this:SetAlpha( 1 - ( ( (this.timer/STEALTHOVERLAY_SHOWTIME) * 100) / 100)  );
+		-- fade out
+		else
+			this:SetAlpha(( (this.timer/STEALTHOVERLAY_SHOWTIME) * 100) / 100);
+		end
+	end
+end	
